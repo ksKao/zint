a7e2346cf40c615d43eba702c7a1eabe19505f3c
Kai Siang Kao <work@ks-kao.com>
2025-10-24 17:57:38 +0800

    Update icon

--- packages/zint-desktop/src/components/navbar.tsx
+++ packages/zint-desktop/src/components/navbar.tsx
@@ -44,7 +44,7 @@ export default function Navbar() {
     <nav className="border-border flex w-72 flex-col justify-between border-r px-4 py-8">
       <div className="flex items-center justify-between">
         <div className="flex w-full items-center gap-4">
-          <img src="/tauri.svg" alt="logo" className="h-8 w-8" />
+          <img src="/icon.png" alt="logo" className="h-8 w-8" />
           <h1 className="text-2xl font-bold">Zint</h1>
         </div>
         <CommandMenu />
